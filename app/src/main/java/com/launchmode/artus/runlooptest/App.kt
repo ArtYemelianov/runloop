93117644c68a173ec0e85c75d1627bfd25cf1d06
Artus <artus91@ukr.net>
2018-08-16 17:20:12 +0300

    Added description for single RssService

--- app/src/main/java/com/launchmode/artus/runlooptest/App.kt
+++ app/src/main/java/com/launchmode/artus/runlooptest/App.kt
@@ -18,7 +18,7 @@ class App : Application() {
         DataBindingUtil.setDefaultComponent(AppDataBindingComponent())
     }
 
-
+    // keep RssService object as single Instance to prevent creation it in different places
     val rssService: RssService
         get() {
             if (_rssService == null || _rssService!!.isDestroyed) {
